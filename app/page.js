630bff03466bb93618d050c529f73e0fd65ba177
justintsugranes <justintsugranes@proton.me>
2022-12-31 12:00:07 -0500

    add hero

--- app/page.js
+++ app/page.js
@@ -1,8 +1,17 @@
-import { Footer, Navbar } from '../components';
-import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
+import { Footer, Navbar } from '../components'
+import {
+  About,
+  Explore,
+  Feedback,
+  GetStarted,
+  Hero,
+  Insights,
+  WhatsNew,
+  World,
+} from '../sections'
 
 const Page = () => (
-  <div>
+  <div className="bg-primary-black overflow-hidden">
     <Navbar />
     <Hero />
     <About />
@@ -14,6 +23,6 @@ const Page = () => (
     <Feedback />
     <Footer />
   </div>
-);
+)
 
-export default Page;
+export default Page
