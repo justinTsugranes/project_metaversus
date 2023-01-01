0d75a821e11cba1a832deb16b50aade4cce103b7
justintsugranes <justintsugranes@proton.me>
2023-01-01 11:27:16 -0500

    add world section

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,9 +1,28 @@
-'use client';
+'use client'
+
+import { motion } from 'framer-motion'
+
+import styles from '../styles'
+import { insights } from '../constants'
+import { InsightCard, TitleText, TypingText } from '../components'
+import { staggerContainer } from '../utils/motion'
 
 const Insights = () => (
-  <section>
-    Insights section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText title="| Insight" textStyles="text-center" />
+      <TitleText
+        title="Insights about the metaverse"
+        textStyles="text-center"
+      />
+    </motion.div>
   </section>
-);
+)
 
-export default Insights;
+export default Insights
